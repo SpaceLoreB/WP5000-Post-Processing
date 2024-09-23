4a6650d1284d0a172dfc053f121dc27eb85c51e1
Lorenzo Becce <lore.becce@gmail.com>
2024-09-23 10:45:18 +0200

    minor changes

--- plots.m
+++ plots.m
@@ -1,19 +1,9 @@
+% This script allows to compare (raw) datasets
 %% Comparative plots
 % f1 = WPcompPlot(C1L,C1R,h_w,'C1');
 % f1 = WPcompPlot(C2L,C2R,h_w,'C2');
 f3 = WPcompPlot(S4C3,S4C3,h_w,'C3H');
 
-%% Plotting (1)
-figure
-v_h_max = max(strNvx,[],2);
-
-area(y,q_h,'FaceAlpha',0.5,'FaceColor',[0.47 0.67 0.19]);   % total flow
-hold on
-grid minor
-area(y,q_us,'FaceColor',[0.07 0.62 1]); % usable flow
-plot([0 0; 4000 4000], [mu*1.25 0.75*mu; mu*1.25 0.75*mu],'k--')
-plot([h_w h_w].*10,[0 2000],'Color','r','LineWidth',1.5)
-bar(y,v_h_max*1e2,'BarWidth',0.2,'FaceColor',[0.72 0.27 1])
 %% Plotting
 % b = rawBar3(rawVelocities);
 % b = rawBar3(v_cutoff);
